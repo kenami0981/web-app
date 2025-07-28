1912e9a6d7e15dfcb32b0b7a53b7e54e588a6a5b
kenami0981 <amelia.janek.aj@gmail.com>
2025-07-28 08:38:56 +0200

    Fonts

---
+++ sideJS.js
@@ -0,0 +1,27 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const sky = document.getElementById("sky");
+  const cloudNum = 25;
+
+  function createClouds() {
+    const cloud = document.createElement("img");
+    cloud.src = "images/clouds.png";
+    cloud.classList.add("cloud");
+
+    const randomSize = Math.random() * 400 + 800;
+    cloud.style.width = randomSize + "px";
+
+    const randomTop = Math.random() * window.innerHeight;
+    cloud.style.top = randomTop + "px";
+
+    const duration = Math.random() * 30 + 60;  // 40-70s
+    const delay = Math.random() * 90;           // 0-30s
+
+    cloud.style.animation = `moveLeft ${duration}s linear ${delay}s infinite`;
+
+    sky.appendChild(cloud);
+  }
+
+  for (let i = 0; i < cloudNum; i++) {
+    createClouds();
+  }
+});
